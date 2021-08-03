40c9efc48c502afc9950a68247a18dda71332cc5
zhengmao_chai <chai.zm@asiainfo-sec.com>
2021-08-03 23:38:46 +0800

    test commit

---
+++ log/src/main/java/com/cimu/jul/Test.java
@@ -0,0 +1,12 @@
+package com.cimu.jul;
+
+/**
+ * @author: zhengmao_chai
+ * @date: 2021/8/3 23:37
+ * @since: 1.0.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test.main");
+    }
+}
